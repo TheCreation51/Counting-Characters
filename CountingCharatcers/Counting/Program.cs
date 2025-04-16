b8562b6bae4f566d94695cda7d51695a3b2b890a
TheCreation51 <79374439+TheCreation51@users.noreply.github.com>
2025-04-16 12:10:21 -0500

    Add files via upload

---
+++ CountingCharatcers/Counting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+static void Main(string[] args)
+{
+Console.WriteLine("Enter a string:");
+string input = Console.ReadLine();
+Dictionary<char, int> charCounts = new Dictionary<char, int>();
+foreach (char c in input)
+{
+char lowerChar = char.ToLower(c);
+if (char.IsLetter(lowerChar))
+{
+if (charCounts.ContainsKey(lowerChar))
+{
+charCounts[lowerChar]++;
+}
+else
+{
+charCounts[lowerChar] = 1;
+}
+}
+}
+Console.WriteLine("Character counts:");
+foreach (var entry in charCounts)
+{
+Console.WriteLine($"{entry.Key}: {entry.Value}");
+}
+}
+}
